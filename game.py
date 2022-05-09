d46e930278d21418ff6b24d6a034aeec12e8cda3
Paloma Soltys <pfs601@gmail.com>
2022-05-09 19:37:57 -0400

    Modifying verifyInput function

--- game.py
+++ game.py
@@ -4,31 +4,26 @@ def main():
     print("Try to guess my number.")
     import random
     answer = random.randint(1,100)
-    guess=verifyInput()
-        
+    guess=validInput()      
     count = 1
     while guess != answer:
         if guess > answer:
-            print("Your guess is too high, try again") 
-            guess=verifyInput()
-            count+=1
+            print("Your guess is too high, try again")         
         elif guess < answer:   
             print("Your guess is too low, try again") 
-            guess=verifyInput()
-            count+=1
+        guess=validInput()
+        count+=1
     print(f"Well done, {username}! You found my number in {count} tries!")        
     print("See you next time!")
 
-def verifyInput():
-    try:
-        guess = int(input("Your guess? "))
-        while guess > 100 or guess < 1:
-            guess = int(input("Your guess has to be between 1 and 100.\nYour guess: "))
-
-    except ValueError:
-        guess =int (input("Your guess has to be an integer.\n Your guess? "))
-        while guess > 100 or guess < 1:
-            guess = int(input("Your guess has to be between 1 and 100.\nYour guess: "))
-
-    return guess
+def validInput():
+    valid_input=False
+    while not valid_input:
+        try:
+            guess = int(input("Your guess? (Enter an integer between 1 and 100) "))
+            if type(guess) == int and 1 <= guess <= 100:
+                valid_input = True 
+        except ValueError:
+            continue
+    return guess       
 main()
